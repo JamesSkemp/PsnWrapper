074e995ae3acf3b398a894f5a385952cc6f3eea4
James Skemp <strivinglife@gmail.com>
2013-08-20 20:27:38 -0500

    Simplified request logic.

--- PsnWrapper/Client.cs
+++ PsnWrapper/Client.cs
@@ -18,6 +18,8 @@ namespace PsnWrapper
 		public string UserAgent { get; set; }
 		public Profile User { get; set; }
 		public string Test { get; set; }
+		public string TestFirmwareData { get; set; }
+		public string TestJid { get; set; }
 		public string TestProfileData { get; set; }
 		public string TestTrophyData { get; set; }
 
@@ -30,21 +32,43 @@ namespace PsnWrapper
 			this.UserAgent = configSettings["UserAgent"];
 		}
 
+		public string ApiRequest(string uri, ICredentials credentials = null, string postData = null)
+		{
+			var request = WebRequest.CreateHttp(uri);
+			request.UserAgent = this.UserAgent;
+			request.Timeout = 10000;
+
+			if (credentials != null)
+			{
+				request.Credentials = credentials;
+				if (!string.IsNullOrWhiteSpace(postData))
+				{
+					request.ContentType = "text/xml; charset=UTF-8";
+					request.Method = "POST";
+					using (var s = request.GetRequestStream())
+					{
+						using (StreamWriter stw = new StreamWriter(s))
+						{
+							stw.Write(postData);
+						}
+					}
+				}
+			}
+
+			using (var response = request.GetResponse())
+			{
+				var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+				return reader.ReadToEnd();
+			}
+		}
+
 		/// <summary>
 		/// Attempt to update the firmware version with the latest information.
 		/// </summary>
 		/// <returns></returns>
 		public bool GetCurrentFirmware()
 		{
-			var request = WebRequest.CreateHttp("http://fus01.ps3.update.playstation.net/update/ps3/list/us/ps3-updatelist.txt");
-			request.UserAgent = this.UserAgent;
-			request.Timeout = 10000;
-
-			using (var response = request.GetResponse())
-			{
-				var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
-				this.Test = reader.ReadToEnd();
-			}
+			this.TestFirmwareData = ApiRequest("http://fus01.ps3.update.playstation.net/update/ps3/list/us/ps3-updatelist.txt");
 			if (!string.IsNullOrWhiteSpace(this.Test))
 			{
 				return true;
@@ -60,34 +84,15 @@ namespace PsnWrapper
 				return false;
 			}
 
+			var credentials = this.BasicLogin.Split(':');
+			var networkCredentials = new System.Net.NetworkCredential(credentials[0], credentials[1]);
+			var postData = string.Format("<?xml version='1.0' encoding='utf-8'?><searchjid platform='ps3' sv='{0}'><online-id>{1}</online-id></searchjid>", this.Firmware, this.User.PsnId);
 			foreach (KeyValuePair<string, string> searchUri in Uris.JidSearch)
 			{
-				var request = WebRequest.CreateHttp(searchUri.Value);
-				request.UserAgent = this.UserAgent;
-				request.Timeout = 10000;
-				request.ContentType = "text/xml; charset=UTF-8";
-				request.Method = "POST";
+				this.TestJid = ApiRequest(searchUri.Value, networkCredentials, postData);
+				var xml = XDocument.Parse(this.TestJid);
+				this.User.Jid = xml.Root.Element("jid").Value;
 
-				var credentials = this.BasicLogin.Split(':');
-				request.Credentials = new System.Net.NetworkCredential(credentials[0], credentials[1]);
-
-				var postData = string.Format("<?xml version='1.0' encoding='utf-8'?><searchjid platform='ps3' sv='{0}'><online-id>{1}</online-id></searchjid>", this.Firmware, this.User.PsnId);
-
-				using (var s = request.GetRequestStream())
-				{
-					using (StreamWriter stw = new StreamWriter(s))
-					{
-						stw.Write(postData);
-					}
-				}
-
-				using (var response = request.GetResponse())
-				{
-					var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
-					this.Test = reader.ReadToEnd();
-					var xml = XDocument.Parse(this.Test);
-					this.User.Jid = xml.Root.Element("jid").Value;
-				}
 				if (!string.IsNullOrWhiteSpace(this.User.Jid))
 				{
 					return true;
@@ -110,33 +115,11 @@ namespace PsnWrapper
 			}
 
 			var searchUri = string.Format("http://getprof.{0}.np.community.playstation.net/basic_view/func/get_profile", this.User.Region);
-
-			var request = WebRequest.CreateHttp(searchUri);
-			request.UserAgent = this.UserAgent;
-			request.Timeout = 10000;
-			request.ContentType = "text/xml; charset=UTF-8";
-			request.Method = "POST";
-
 			var credentials = this.BasicLogin.Split(':');
-			request.Credentials = new System.Net.NetworkCredential(credentials[0], credentials[1]);
-
+			var networkCredentials = new System.Net.NetworkCredential(credentials[0], credentials[1]);
 			var postData = string.Format("<profile platform='ps3' sv='{0}'><jid>{1}</jid></profile>", this.Firmware, this.User.Jid);
 
-			using (var s = request.GetRequestStream())
-			{
-				using (StreamWriter stw = new StreamWriter(s))
-				{
-					stw.Write(postData);
-				}
-			}
-
-			using (var response = request.GetResponse())
-			{
-				var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
-				this.TestProfileData = reader.ReadToEnd();
-				//var xml = XDocument.Parse(this.Test);
-				//this.User.Jid = xml.Root.Element("jid").Value;
-			}
+			this.TestProfileData = ApiRequest(searchUri, networkCredentials, postData);
 
 			return true;
 		}
@@ -150,35 +133,14 @@ namespace PsnWrapper
 			}
 
 			var searchUri = "http://trophy.ww.np.community.playstation.net/trophy/func/get_user_info";
-
-			var request = WebRequest.CreateHttp(searchUri);
-			request.UserAgent = this.UserAgent;
-			request.Timeout = 10000;
-			request.ContentType = "text/xml; charset=UTF-8";
-			request.Method = "POST";
-
+			// todo, move into static?
 			var credentials = this.TrophyLogin.Split(':');
 			// Yuck.
-			request.Credentials = new System.Net.NetworkCredential(credentials[0], this.TrophyLogin.Replace(credentials[0] + ":", ""));
-
+			var networkCredentials = new System.Net.NetworkCredential(credentials[0], this.TrophyLogin.Replace(credentials[0] + ":", ""));
 			// todo, start and end range - doesn't need to be 64?
 			var postData = string.Format("<nptrophy platform='ps3' sv='{0}'><jid>{1}</jid><start>1</start><max>64</max><pf>ps3</pf><pf>psp2</pf></nptrophy>", this.Firmware, this.User.Jid);
 
-			using (var s = request.GetRequestStream())
-			{
-				using (StreamWriter stw = new StreamWriter(s))
-				{
-					stw.Write(postData);
-				}
-			}
-
-			using (var response = request.GetResponse())
-			{
-				var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
-				this.TestTrophyData = reader.ReadToEnd();
-				//var xml = XDocument.Parse(this.Test);
-				//this.User.Jid = xml.Root.Element("jid").Value;
-			}
+			this.TestTrophyData = ApiRequest(searchUri, networkCredentials, postData);
 
 			return true;
 		}
