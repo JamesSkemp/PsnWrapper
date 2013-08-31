736ce716abe08c19952eb83c6574e88668eda744
James Skemp <strivinglife@gmail.com>
2013-08-31 16:37:35 -0500

    Added new login class for client cleanup.
    Began adding documentation.

---
+++ PsnWrapper/Login.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsnWrapper
+{
+	/// <summary>
+	/// Login credentials when connecting to the API.
+	/// </summary>
+	public class Login
+	{
+		/// <summary>
+		/// Username to use.
+		/// </summary>
+		public string Username { get; set; }
+		/// <summary>
+		/// Password to use.
+		/// </summary>
+		public string Password { get; set; }
+		/// <summary>
+		/// Return whether the login information has both a username and password assigned.
+		/// </summary>
+		public bool IsPopulated
+		{
+			get
+			{
+				return !string.IsNullOrWhiteSpace(this.Username) && !string.IsNullOrWhiteSpace(this.Password);
+			}
+		}
+
+		public Login()
+		{
+		}
+
+		public Login(string username, string password)
+			: this()
+		{
+			this.Username = username;
+			this.Password = password;
+		}
+	}
+}
--- PsnWrapper/Profile.cs
+++ PsnWrapper/Profile.cs
@@ -8,8 +8,17 @@ namespace PsnWrapper
 {
 	public class Profile
 	{
+		/// <summary>
+		/// PlayStation Network id of the user to get data for.
+		/// </summary>
 		public string PsnId { get; set; }
+		/// <summary>
+		/// JID of the user. Formatted like an email.
+		/// </summary>
 		public string Jid { get; set; }
+		/// <summary>
+		/// Region of the user.
+		/// </summary>
 		public string Region { get; set; }
 
 		public Profile()
